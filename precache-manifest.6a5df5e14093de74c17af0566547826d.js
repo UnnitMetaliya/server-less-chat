fc3fcc48db522c0e6783cb5ad3b92ee38f857299
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-23 14:00:12 -0400

    Updates

---
+++ precache-manifest.6a5df5e14093de74c17af0566547826d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f991075f1807eb8d473002b699733a5e",
+    "url": "/server-less-chat/index.html"
+  },
+  {
+    "revision": "51bea73dd591a2c02112",
+    "url": "/server-less-chat/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1eb0711b64e07c65d872",
+    "url": "/server-less-chat/static/js/2.086d6ff9.chunk.js"
+  },
+  {
+    "revision": "51bea73dd591a2c02112",
+    "url": "/server-less-chat/static/js/main.2d1131fa.chunk.js"
+  },
+  {
+    "revision": "2aff42ebbb3700f58561",
+    "url": "/server-less-chat/static/js/runtime~main.6077172e.js"
+  }
+]);
